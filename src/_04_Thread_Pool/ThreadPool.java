7529d348c1258c2b95ab600f3f62c3b6605edaf1
sepfish <league@wts7.attlocal.net>
2019-01-19 14:05:49 -0800

    almost finshed 03 and started 04

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,22 @@
 package _04_Thread_Pool;
 
-public class ThreadPool {
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+import javax.swing.JOptionPane;
+
+public class ThreadPool {
+	Thread[] threads;
+	ConcurrentLinkedQueue<Task> taskQueue;
+	public
+public ThreadPool(int totalThreads)
+{
+		threads = Thread[totalThreads];
+		for(int i =0;i<threads.length;i++)
+		{
+			threads[i].add
+		}
+		}
+	
+
+	
 }
