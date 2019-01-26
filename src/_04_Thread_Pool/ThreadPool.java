8444db6a4cafbd3dc648121cba01e5f5798366e3
sepfish <league@wts7.attlocal.net>
2019-01-26 14:29:50 -0800

    worked on threadpool

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,21 +1,36 @@
 package _04_Thread_Pool;
-
 import java.util.concurrent.ConcurrentLinkedQueue;
-
 import javax.swing.JOptionPane;
-
 public class ThreadPool {
 	Thread[] threads;
 	ConcurrentLinkedQueue<Task> taskQueue;
-	public
 public ThreadPool(int totalThreads)
 {
-		threads = Thread[totalThreads];
+	threads = new Thread[totalThreads];
+	taskQueue = new ConcurrentLinkedQueue<Task>();
 		for(int i =0;i<threads.length;i++)
 		{
-			threads[i].add
+			threads[i] = new Thread(new Worker(taskQueue));
 		}
 		}
+public void addTask(Task task)
+{
+	taskQueue.add(task);
+	}
+public void start()
+{
+	for(int i =0;i<threads.length;i++)
+	{
+		threads[i].start();
+		try {
+			threads[i].join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	}
 	
 
 	
--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,13 +1,30 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class Worker implements Runnable {
-
+public class Worker implements Runnable, Task{
+	ConcurrentLinkedQueue<Task> taskQueue;
+	public Worker(	ConcurrentLinkedQueue<Task> taskQueue)
+{
+		this.taskQueue = taskQueue;
+		}
 	@Override
 	public void run() {
+		if(taskQueue.isEmpty() == false)
+		{
+			taskQueue.remove().perform();
+			
+		}
+		
+	}
+	@Override
+	public void perform() {
 		// TODO Auto-generated method stub
 		
 	}
+	
+
+	
 
 
 }
